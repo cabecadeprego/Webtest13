5768f0356752335e90ccbd945e55170e0a4e69ea
marcosranes <marcosranes@gmail.com>
2022-03-17 01:15:30 -0300

    modified files

--- build.sh
+++ build.sh
@@ -10,11 +10,7 @@ go mod tidy
 
 CGO_ENABLED="0" go build -ldflags="-s -w" -a -v -o Webtest13 .
 
-pushd ui
-rice append --exec ../Webtest13
-popd
+pushd ui+-
 
-cp Webtest13 dist/
-cp conf.toml dist/
 
 rm Webtest13
